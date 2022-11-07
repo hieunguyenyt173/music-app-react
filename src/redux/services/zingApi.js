a9184e5a6ffbdb8197df0e392c4a0271b4f60863
Nguyễn Văn Hiếu <104955838+hieunguyenyt173@users.noreply.github.com>
2022-11-07 11:48:55 +0700

    update

--- src/redux/services/zingApi.js
+++ src/redux/services/zingApi.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const zingApi = createApi({
   reducerPath: "zingApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "",
+    baseUrl: "https://apizingmp3.herokuapp.com",
   }),
 
   endpoints: (builder) => ({
