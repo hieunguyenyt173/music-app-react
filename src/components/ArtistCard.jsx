78299bbda84fadacfa8005311b0138794ce8f1c5
Nguyễn Văn Hiếu <104955838+hieunguyenyt173@users.noreply.github.com>
2022-11-15 14:42:47 +0700

    update

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -9,7 +9,7 @@ function ArtistCard({artist}) {
       <div className="w-32 h-32 rounded-full overflow-hidden mx-auto">
         <img
         className="group-hover:scale-110 transition-all"
-          src={artist.thumbnailM}
+          src={artist.thumbnail}
           alt=""
         />
       </div>
