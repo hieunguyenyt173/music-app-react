2aba6914cdc3898ff9e88f3d41c61d5f81bd9d9a
Hieu Nguyen <hieunguyenyt173@gmail.com>
2022-11-17 23:20:15 +0700

    update

--- src/redux/services/userApi.js
+++ src/redux/services/userApi.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const userApi = createApi({
   reducerPath: "userApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3001/",
+    baseUrl: "https://demo-kmusic-api.herokuapp.com",
   }),
 
   endpoints: (builder) => ({
